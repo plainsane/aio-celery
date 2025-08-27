bdfe49a06fc636559d37a75e4a855d927b9d9a74
Ashley Stovall <plainsane+github@gmail.com>
2025-08-27 12:11:38 -0400

    bad import

--- aio_celery/__init__.py
+++ aio_celery/__init__.py
@@ -4,8 +4,9 @@ from ._state import get_current_app as current_app
 from .app import Celery, shared_task
 from .canvas import chain, signature
 from .task import Task
+from .cli import create_parser, create_worker_parser
 
-__version__ = "0.22.0"
+__version__ = "0.22.0.dev1"
 
 __all__ = (
     "Celery",
@@ -14,4 +15,6 @@ __all__ = (
     "current_app",
     "shared_task",
     "signature",
+    "create_parser",
+    "create_worker_parser",
 )
